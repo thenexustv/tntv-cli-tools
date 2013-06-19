a698425bdc116309a8c79cdbb9503519aabb87f2
Ryan Rampersad <ryan.rampersad@gmail.com>
2013-06-19 11:18:52 -0500

    Fixing display -e issue with echo's in setup.sh

--- setup.sh
+++ setup.sh
@@ -9,5 +9,5 @@ sudo pip install boto
 sudo cp config-aws.yaml.sample config-aws.yaml
 sudo cp config-meta.yaml.sample config-meta.yaml
 echo "Please populate "
-echo -e "\t config-aws.yaml with proper tokens for AWS authentication"
-echo -e "\t config-meta.yaml with proper paths your composer name\n\t You will need a folder containing album as well!"
+echo "\t config-aws.yaml with proper tokens for AWS authentication.\n"
+echo "\t config-meta.yaml with proper paths your composer name\n\t You will need a folder containing album art as well; contact Ryan.\n"
